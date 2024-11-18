21dc02602662841dd099b5e0577b3a6de3a405fd
carri1608 <carri1608@gmail.com>
2024-11-18 10:34:05 +0100

    Introduccion trabajo POO

---
+++ src/Ejemplos/Persona.java
@@ -0,0 +1,17 @@
+package Ejemplos;
+
+
+//Se ha creado una clase que se llama Persona
+public class Persona {
+
+    //Sus atributos serán nombre y edad
+
+    //El atributo nombre es un String
+    String nombre;
+
+    //El atributo edad es un Integer
+    int edad;
+
+
+
+}
---
+++ src/POO.java
@@ -0,0 +1,7 @@
+public class POO {
+
+    public static void main(String[] args) {
+
+    }
+
+}
